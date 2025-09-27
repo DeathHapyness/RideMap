3d25e32dbc417ccd7a8c47d1f37c9aa84a4d38db
DeathHapyness <rique.bcavriani@proton.me>
2025-09-27 00:01:25 -0300

    criado banco de dados com suas configuracoes e especifiacoes para melhor desempenho e todas as tables criadas.

--- db/config.js
+++ db/config.js
@@ -1,1 +1,21 @@
-//TERMINAR DE CONFIGURAR BANCO DE DADOS COM SUAS DEPENDENCIAS 
+//TERMINAR DE CONFIGURAR BANCO DE DADOS COM SUAS DEPENDENCIAS  
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados MySQL!');
+});
+
+module.exports = connection;
+
