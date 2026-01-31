40b49e5e2417d791dfa9d74d248619a731a7f89b
DeathHapyness <rique.bcavriani@proton.me>
2026-01-30 21:14:07 -0300

    test:testando cloud

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,9 @@
 const cloudinary = require('cloudinary').v2;
-    console.log('Cloudinary configurado com sucesso'); 
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+module.exports = cloudinary;
