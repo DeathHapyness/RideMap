c3eb7aee2b5269da943abcd8e2ebf93d5ac295ce
DeathHapyness <rique.bcavriani@proton.me>
2026-01-30 21:07:45 -0300

    feat:criado arquivos e interface para chatbot

---
+++ static/js/chat-Bot.js
@@ -0,0 +1,8 @@
+function showChatBot() {
+  document.getElementById("chatBotModal").style.display = "block";
+  console.log("ChatBot opened");
+}
+
+function closeChatBot() {
+  document.getElementById("chatBotModal").style.display = "none";
+}
