7467326baed7c14d41d1af3d2fd56a29d0376456
DeathHapyness <rique.bcavriani@proton.me>
2026-02-05 22:34:57 -0300

    fix: corrigido erros de configuracao do cloud

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,22 @@
+require('dotenv').config();  // PRIMEIRO!
+
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true
 });
+
+const imageUrl = cloudinary.url('background-404_qslrji', {
+  transformation: [
+    { quality: 'auto' },
+    { fetch_format: 'auto' },
+    { width: 1200 }
+  ]
+});
+
+console.log('Image URL:', imageUrl);
 
 module.exports = cloudinary;
